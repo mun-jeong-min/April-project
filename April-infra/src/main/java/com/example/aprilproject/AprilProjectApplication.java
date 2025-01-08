85781aa6dcadd0221de63e824ab4528a2c05f19a
문정민 <moondeve41@gmail.com>
2025-01-08 20:48:25 +0900

    gradle setting

--- April-infra/src/main/java/com/example/aprilproject/AprilProjectApplication.java
+++ April-infra/src/main/java/com/example/aprilproject/AprilProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AprilProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AprilProjectApplication.class, args);
     }
-
 }
